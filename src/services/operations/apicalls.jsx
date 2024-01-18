438e25072f554c6f53cc018ea659699f0edd4677
sarvajeet-hue <75826972+sarvajeet-hue@users.noreply.github.com>
2024-01-18 14:53:51 +0530

    updated commit on send otp func

--- src/services/operations/apicalls.jsx
+++ src/services/operations/apicalls.jsx
@@ -73,16 +73,33 @@ export const sendOtp = (data , navigate) => {
             
             console.log("Data.email-->",data.email)
             const email = data.email
-            const response = axios.post("http://localhost:4000/api/v1/auth/otp" ,{email} )
+            const response = await axios.post("http://localhost:4000/api/v1/auth/otp" ,{email} )
             console.log(response)
 
             
             
+            if(!response.data.success){
+                  
+                throw new Error(response.data.message) 
+               
+             
+            }
+            
+            
+            
             navigate("/verifymail")
         }
-        catch(error){
-            console.log(error) 
-            toast.error(error.response.data.message)  
+        catch (error) {
+            // Display toast error message from the API response
+            if (error.response) {
+                toast.error(error.response.data.message);
+                
+                
+            } else {
+                // Display a generic error message for other errors
+                toast.error("An error occurred during login");
+            }
+            console.error("Login error:", error);
         }
     }
 }
