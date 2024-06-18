ce93ba547d2376c13e55c773813f9889c2f41e96
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-06-18 12:27:27 +0530

    Upload image input fixed

--- src/DashboardPage/setting/SettingImageUpload.jsx
+++ src/DashboardPage/setting/SettingImageUpload.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setImage } from '../../Slices/auth';
+import { FaUpload } from "react-icons/fa";
+
 
 const SettingImageUpload = () => {
 
@@ -28,7 +30,7 @@ const SettingImageUpload = () => {
 
   return (
     <div className='bg-richblack-700 w-full h-[400px]'>
-        <div className='flex gap-[20px] '>
+        <div className='flex gap-[20px] p-[20px] '>
              <img className='w-[80px] h-[80px] rounded-full'
               src={selectFile ? selectFile : image} alt="" />
              
@@ -40,7 +42,13 @@ const SettingImageUpload = () => {
                     {/* <button
 
                      className='bg-yellow-5 rounded-md px-3 py-1 text-richblack-800 font-inter font-semibold'>Change</button> */}
-                      <input onChange={selectHandler }  className='bg-yellow-5 leading-tight  focus:outline-none appearance-none rounded-md px-3 py-1 text-richblack-800 font-inter font-semibold' placeholder='Select' type="file"  />
+                      {/* <input onChange={selectHandler }  className='bg-yellow-5 leading-tight  focus:outline-none appearance-none rounded-md px-3 py-1 text-richblack-800 font-inter font-semibold' placeholder='Select' type="file"  /> */}
+                     <div className='bg-yellow-5 leading-tight focus:outline-none appearance-none rounded-full flex items-center justify-center h-[40px] w-[40px] px-3 py-1 text-richblack-800'>
+                      <input type="file" id='ImageFile' onChange={selectHandler} className='appearance-none hidden'  />
+                      <label htmlFor="ImageFile" className='rounded-lg flex items-center justify-center' >
+                      <FaUpload/>
+                      </label>
+                     </div>
                       <button  onClick={imageHandler} className='bg-richblack-500 rounded-md px-3 py-1 text-richblack-800 font-inter font-semibold' >Update</button>
                     <button className='bg-richblack-500 rounded-md px-3 py-1 text-richblack-800 font-inter font-semibold'>Remove</button>
                 </div>
